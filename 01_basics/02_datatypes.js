2be684feb83cfeb1d5034897dd608a2f731ea31a
Bahubali Desai <bahubalidesai0@gmail.com>
2025-08-27 13:27:48 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // treate all javascript code as newer version
+
+//alert(3+3) //we are using node js, not browser
+
+console.log(3
+    +3); // code readability should be high
+
+console.log("bahubali");
+
+let name = "bahubali"
+let age = 33
+let isloggedin = false
+
+// number => 2 to power of 53
+// bigint => 
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined =>
+// symbol  => unique
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null)// object
