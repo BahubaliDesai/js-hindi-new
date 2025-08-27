1c7760dab0ee4205b593da72621d2951ad0078d2
Bahubali Desai <bahubalidesai0@gmail.com>
2025-08-27 13:46:59 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+let score = null;
+
+//console.log(typeof score);
+
+
+let stringtoNumber = Number(score);
+//console.log(stringtoNumber)
+//console.log(typeof stringtoNumber);
+
+
+// "33" = 33
+// "33a" = NaN
+// null = 0
+// undefined = NaN
+// true = 1
+// false = 0
+
+let isloggedin = 1
+
+let booleanLoggedIn = Boolean(isloggedin);
+//console.log(booleanLoggedIn)
+
+// 1 => true; 0=> false
+// "" => false; "bahubali" => trure
+
+let age = 33
+let stringNumber = String(age)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
